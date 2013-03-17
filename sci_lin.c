28a59634f203830919dd587ab53aee0be559d274
Alexandru Burghelea <iceman.ftg@gmail.com>
2013-03-17 12:06:50 +0200

    Trying to intercept open/close

--- sci_lin.c
+++ sci_lin.c
@@ -18,20 +18,45 @@ MODULE_AUTHOR("Alexandru George Burghelea");
 MODULE_LICENSE("GPL");
 
 
-//static long (*f)(struct syscall_params);
+static long (*f)(struct syscall_params);
+static long (*g)(struct syscall_params);
+
+extern void *sys_call_table[];
+extern long my_nr_syscalls;
+
+asmlinkage long interceptor (struct syscall_params sp)
+{
+	int syscall = sp.eax;
+	int r = f(sp);
+	printk (LOG_LEVEL "Open intercept %d %d\n", syscall, r);
+
+	return r;
+}
+
+asmlinkage long interceptor2 (struct syscall_params sp)
+{
+	int syscakk = sp.eax;
+	int r = g(sp);
+	printk (LOG_LEVEL "Close intercept %d %d\n", syscall, r);
+
+	return r;
+}
 
 static int sci_init(void)
 {
 	printk(LOG_LEVEL "SCI Loading\n");
-	//f = sys_call_table[_NR_open];
-	//sys_call_table[_NR_open] = interceptor;
+	f = sys_call_table[__NR_open];
+	g = sys_call_table[__NR_close];
+	sys_call_table[__NR_close] = interceptor2;
+	sys_call_table[__NR_open] = interceptor;
 	return 0;
 }
 
 static void sci_exit(void)
 {
 	printk(LOG_LEVEL "SCI Unloading\n");
-	//sys_call_table[_NR_open] = f;
+	sys_call_table[__NR_open] = f;
+	sys_call_table[__NR_close] = g;
 }
 
 module_init(sci_init);
