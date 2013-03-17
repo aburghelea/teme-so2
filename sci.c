bdabf4b0ccdec85cd5721e0fa6a0de6a131eaf4b
Alexandru Burghelea <iceman.ftg@gmail.com>
2013-03-17 14:03:11 +0200

    Alloc/Dealloc of sys_call_table duplicate

--- sci.c
+++ sci.c
@@ -20,6 +20,8 @@ MODULE_LICENSE("GPL");
 extern void *sys_call_table[];
 extern long my_nr_syscalls;
 
+void **replace_call_table;
+
 asmlinkage long my_syscall(int cmd, long syscall, long pid)
 {
 	printk(LOG_LEVEL "THIS IS ME TRING TO INTERCEPT THE CALLS");
@@ -46,18 +48,38 @@ asmlinkage long my_syscall(int cmd, long syscall, long pid)
 	return 0;
 }
 
-static int sci_init(void)
+static int init_replace_call_table(void)
 {
-	printk(LOG_LEVEL "SCI Loading %ld\n", my_nr_syscalls);
-	sys_call_table[MY_SYSCALL_NO] = my_syscall;
-	return 0;
+	int i;
+	
+	replace_call_table = kmalloc( my_nr_syscalls * sizeof(void *), GFP_KERNEL);
+	if (!replace_call_table)
+		return -ENOMEM;
+
+	for (i = 0 ; i < my_nr_syscalls; i++)
+		replace_call_table[i] = NULL;
+
+	return 0;	
+
 }
 
+static int sci_init(void)
+{
+	int err;
+	printk(LOG_LEVEL "SCI Loading %ld\n", my_nr_syscalls);
+	sys_call_table[MY_SYSCALL_NO] = my_syscall;
+	err = init_replace_call_table();
 
+	if (!err)
+		return err;
+
+	return 0;
+}
 
 static void sci_exit(void)
 {
 	printk(LOG_LEVEL "SCI Unloading\n");
+	kfree(replace_call_table);
 }
 
 module_init(sci_init);
