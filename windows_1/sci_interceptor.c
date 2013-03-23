98f1eb1889f557e5d7b7277daab68e276497c37e
Alexandru Burghelea <iceman.ftg@gmail.com>
2013-03-23 20:27:05 +0200

    Refactored table duplication

--- windows_1/sci_interceptor.c
+++ windows_1/sci_interceptor.c
@@ -3,44 +3,124 @@
 #include "sci_list.h"
 #include "sci_win.h"
 
-void **OriginalServiceTableShadow;
-void **OriginalServiceTable;
+// void **OriginalServiceTableShadow;
+// void **OriginalServiceTable;
+// unsigned char *OriginalSpt;
+// unsigned char *OriginalShadowSpt;
+// int OriginalLs;
 
-void InitServiceDescriptorTable() {
-	
-	void ** newSt;
+struct std OriginalDescriptorTable;
+struct std OriginalDescriptorTableShadow;
+struct std *CurrentDescriptorTable;
+
+int my_syscall (int cmd, int syscall_no, HANDLE pid)
+{	
+	DbgPrint("Entering my syscall");	
+	switch (cmd) {
+	case REQUEST_SYSCALL_INTERCEPT: {
+		DbgPrint("REQUEST_SYSCALL_INTERCEPT\n");
+		//code = start_intercept(syscall);
+		break;
+	}
+	case REQUEST_SYSCALL_RELEASE: {
+		DbgPrint("REQUEST_SYSCALL_RELEASE\n");
+		//code = stop_intercept(syscall);
+		break;
+	}
+	case REQUEST_START_MONITOR: {
+		DbgPrint("REQUEST_START_MONITOR\n");
+		//code = start_monitor(syscall, pid);
+		break;
+	}
+	case REQUEST_STOP_MONITOR: {
+		DbgPrint("REQUEST_STOP_MONITOR\n");
+		//code = stop_monitor(syscall, pid);
+		break;
+	}
+	default:
+		return STATUS_INVALID_PARAMETER;
+	}	
+
+	return STATUS_SUCCESS;
+}
+
+void shallowCopyDT(struct std *destination, struct std *source)
+{
+	destination->st = source->st;
+	destination->ls = source->ls;
+	destination->spt = source->spt;
+}
+
+struct std *allocateDescriptorTableInfo() {
+	struct std *table;
+
 	long no_syscalls = MY_SYSCALL_NO + 1;
 	int i;
-	SIZE_T sts = no_syscalls * sizeof(void *);
-	if (!(newSt = ExAllocatePoolWithTag(NonPagedPool, sts, MEM_TAG)))
-		return;
+	SIZE_T st_size = no_syscalls * sizeof(void *);
+	SIZE_T ct_size = no_syscalls * sizeof(int);
+	SIZE_T spt_size = no_syscalls * sizeof(unsigned char);
 
-	OriginalServiceTable = KeServiceDescriptorTable[0].st;
-	OriginalServiceTableShadow = KeServiceDescriptorTableShadow->st;
+	if (!(table = ExAllocatePoolWithTag(NonPagedPool, sizeof(struct std), MEM_TAG)))
+		return NULL;
 
-	for (i = 0; i <= KeServiceDescriptorTable[0].ls; i++) {
-		newSt[i] = KeServiceDescriptorTable[0].st[i];
+	if (!(table->st = ExAllocatePoolWithTag(NonPagedPool, st_size, MEM_TAG)))
+		return NULL;
+
+	if (!(table->ct = ExAllocatePoolWithTag(NonPagedPool, ct_size, MEM_TAG)))
+		return NULL;
+	
+	if (!(table->spt = ExAllocatePoolWithTag(NonPagedPool, spt_size, MEM_TAG)))
+		return NULL;
+
+
+		return table;	
+}
+
+void deepCopyDescriptoTableInfo( struct std *destination, struct std *source)
+{
+	int i = 0;
+	DbgPrint("%d\n", source->ls);
+		
+	for (i = 0 ; i <= source->ls; i++){
+		DbgPrint("DEEP %d\n", i);
+		destination->st[i] = source->st[i];
+		destination->spt[i] = source->spt[i];
 	}
 
+	destination->st[MY_SYSCALL_NO] = my_syscall;
+	destination->ls = MY_SYSCALL_NO + 1;
+	destination->spt[MY_SYSCALL_NO] = sizeof(int) * 2 + sizeof(HANDLE);
+}
+void InitServiceDescriptorTable() {
+	
+	shallowCopyDT(&OriginalDescriptorTable, &KeServiceDescriptorTable[0]);	
+	shallowCopyDT(&OriginalDescriptorTableShadow, KeServiceDescriptorTableShadow);
+
+	CurrentDescriptorTable = allocateDescriptorTableInfo();
+
+	deepCopyDescriptoTableInfo(CurrentDescriptorTable, &OriginalDescriptorTable);
 	WPON();
-	KeServiceDescriptorTable[0].st = newSt;
-	KeServiceDescriptorTableShadow->st = newSt;
+	shallowCopyDT(&KeServiceDescriptorTable[0],CurrentDescriptorTable);
+	KeServiceDescriptorTableShadow = CurrentDescriptorTable;
 	WPOFF();
 }
 
 void CleanServiceDescriptorTable(){
 	WPON();
-	if (OriginalServiceTable != NULL)
+	if (CurrentDescriptorTable != NULL)
 	{
-		KeServiceDescriptorTable[0].st = OriginalServiceTable;
-		KeServiceDescriptorTableShadow->st = OriginalServiceTableShadow;	
+	
+		shallowCopyDT(&KeServiceDescriptorTable[0], &OriginalDescriptorTable);
+		KeServiceDescriptorTableShadow = &OriginalDescriptorTableShadow;
+	
 	}
 	WPOFF();
 }
 
+
 void DriverUnload(PDRIVER_OBJECT driver)
 {
-	CleanServiceDescriptorTable();
+	// CleanServiceDescriptorTable();
 	
 	return;
 }
