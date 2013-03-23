b359e6fea3aa1acb2447562284b26e7ce112fc3d
Alexandru Burghelea <iceman.ftg@gmail.com>
2013-03-23 23:35:27 +0200

    Refactored sycall from void* to int

--- windows_1/sci_list.h
+++ windows_1/sci_list.h
@@ -7,21 +7,21 @@ struct sci_info
 {
 	SINGLE_LIST_ENTRY list;
 	HANDLE pid;
-	void *syscall;
+	int syscall;
 
 };
 
 void sci_info_init();
 
-NTSTATUS sci_info_add(void *sycall, HANDLE pid);
+NTSTATUS sci_info_add(int sycall, HANDLE pid);
 
 NTSTATUS sci_info_remove_for_pid (HANDLE pid);
 
-NTSTATUS sci_info_remove_for_syscall(void *syscall);
+NTSTATUS sci_info_remove_for_syscall(int syscall);
 
-NTSTATUS sci_info_remove_for_pid_syscall(void *syscall, HANDLE pid);
+NTSTATUS sci_info_remove_for_pid_syscall(int syscall, HANDLE pid);
 
-BOOLEAN sci_info_contains_pid_syscall(void *syscall, HANDLE pid);
+BOOLEAN sci_info_contains_pid_syscall(int syscall, HANDLE pid);
 
 void destroy_list(void);
 
